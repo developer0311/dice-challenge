df8c5a3466e2ee571e47b220ba1af1ed3b303293
Diprati Das <102643011+developer0311@users.noreply.github.com>
2023-05-05 23:58:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+var randomImageSource1 = "dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src" , randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
 
 if (randomNumber1 > randomNumber2)
